8f8695a523b114bcdb4271675d2c337296cfc344
SaitoF3110 <water.tokotoko.drive@gmail.com>
2023-11-01 20:42:43 +0900

    アニメーション切り替えの実験 上手くいきそう

---
+++ Assets/script/SS6Script/kirikae.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kirikae : MonoBehaviour
+{
+    [SerializeField] GameObject _stay;
+    [SerializeField] GameObject _attack1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            foreach (Transform child in this.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+            Instantiate(_stay,this.transform);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            foreach (Transform child in this.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+            Instantiate(_attack1, this.transform);
+        }
+    }
+}
